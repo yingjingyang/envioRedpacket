5c3f3d1ee7f89b68f7d8385b737e50808db2ca8a
gongtao245 <gongtao245@gmail.com>
2024-08-10 11:50:26 +0800

    fix handler logic

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -3,51 +3,216 @@
  */
 import {
   HappyRedPacketContract,
-  HappyRedPacket_ClaimSuccessEntity,
-  HappyRedPacket_CreationSuccessEntity,
-  HappyRedPacket_RefundSuccessEntity,
+  ClaimEntity,
+  RedpacketEntity,
+  RefundEntity,
+  LastupdateEntity,
+  TokenEntity,
 } from "generated";
 
+import { GetClient } from "./utils/client";
+
+const Erc20ABI = [
+  {
+    name: "decimals",
+    type: "function",
+    inputs: [],
+    outputs: [{ name: "", type: "uint8" }],
+  },
+  {
+    name: "symbol",
+    type: "function",
+    inputs: [],
+    outputs: [{ name: "", type: "string" }],
+  },
+  {
+    name: "name",
+    type: "function",
+    inputs: [],
+    outputs: [{ name: "", type: "string" }],
+  },
+];
+
+
+HappyRedPacketContract.ClaimSuccess.loader(({event,context}) => {
+  context.Redpacket.load(event.params.id,undefined);
+})
+
 HappyRedPacketContract.ClaimSuccess.handler(({ event, context }) => {
-  const entity: HappyRedPacket_ClaimSuccessEntity = {
-    id: `${event.transactionHash}_${event.logIndex}`,
-    event_id: event.params.id,
+  let claimID = event.transactionHash + event.logIndex
+
+  const claimEntity: ClaimEntity = {
+    id: claimID,
+    happyRedPacketId: event.params.id,
     claimer: event.params.claimer,
-    claimed_value: event.params.claimed_value,
-    token_address: event.params.token_address,
+    claimedValue: event.params.claimed_value,
+    tokenAddress: event.params.token_address,
     lock: event.params.lock,
+    blockNumber: BigInt(event.blockNumber),
+    blockTimestamp: BigInt(event.blockTimestamp),
+    transactionHash: event.transactionHash,
+    redpacket_id: event.params.id,
   };
 
-  context.HappyRedPacket_ClaimSuccess.set(entity);
+  let redpacket = context.Redpacket.get(event.params.id);
+  if (redpacket == undefined) {
+    return
+  }
+  
+  let tempBigInt = redpacket.remainToClaim - BigInt(1)
+  let checkAllClaimed = false
+  if(tempBigInt == BigInt(0)){
+    checkAllClaimed = true
+  }
+
+  context.Redpacket.set({
+    id: event.params.id,
+    total: redpacket.total,
+    happyRedPacketId: redpacket.happyRedPacketId,
+    name: redpacket.name,
+    message: redpacket.message,
+    creator: redpacket.creator,
+    creationTime: redpacket.creationTime,
+    tokenAddress: redpacket.tokenAddress,
+    token_id: redpacket.token_id,
+    number: redpacket.number,
+    remainToClaim: redpacket.remainToClaim,
+    ifrandom: redpacket.ifrandom,
+    duration: redpacket.duration,
+    lock: redpacket.lock,
+    blockNumber: redpacket.blockNumber,
+    blockTimestamp: redpacket.blockTimestamp,
+    transactionHash: redpacket.transactionHash,
+    expireTimestamp: redpacket.expireTimestamp,
+    refunded: redpacket.refunded,
+    refunder_id: redpacket.refunder_id,
+    allClaimed: checkAllClaimed,
+  });
+
+  context.Lastupdate.set({
+    id: "1",
+    lastupdateTimestamp: BigInt(event.blockTimestamp),
+  })
+
+  context.Claim.set(claimEntity);
 });
 
-HappyRedPacketContract.CreationSuccess.handler(({ event, context }) => {
-  const entity: HappyRedPacket_CreationSuccessEntity = {
-    id: `${event.transactionHash}_${event.logIndex}`,
+HappyRedPacketContract.CreationSuccess.loader(({event,context}) => {
+  context.Redpacket.load(event.params.id,undefined);
+  context.Token.load(event.params.token_address)
+})
+
+
+HappyRedPacketContract.CreationSuccess.handlerAsync(async ({ event, context }) => {
+  context.Lastupdate.set({
+    id: "1",
+    lastupdateTimestamp: BigInt(event.blockTimestamp),
+  })
+
+  const redpacketEntity: RedpacketEntity = {
+    id: event.params.id,
     total: event.params.total,
-    event_id: event.params.id,
+    happyRedPacketId: event.params.id,
     name: event.params.name,
     message: event.params.message,
     creator: event.params.creator,
-    creation_time: event.params.creation_time,
-    token_address: event.params.token_address,
+    creationTime: event.params.creation_time,
+    tokenAddress: event.params.token_address,
+    token_id: event.params.token_address,
     number: event.params.number,
+    remainToClaim: event.params.number,
     ifrandom: event.params.ifrandom,
     duration: event.params.duration,
     lock: event.params.lock,
+    blockNumber: BigInt(event.blockNumber),
+    blockTimestamp: BigInt(event.blockTimestamp),
+    transactionHash: event.transactionHash,
+    expireTimestamp: event.params.creation_time + event.params.duration,
+    refunded: false,
+    refunder_id: undefined,
+    allClaimed: false,
   };
 
-  context.HappyRedPacket_CreationSuccess.set(entity);
+  let token = await context.Token.get(event.params.token_address);
+  const chainId = event.chainId
+  const eventId = `${chainId}-${event.params.id}`;
+
+  if (token == null) {
+    try{
+      const client = GetClient(chainId);
+
+      const tokenName = (await client.readContract({
+        address: event.params.token_address as any,
+        abi: Erc20ABI,
+        functionName: "name",
+        args: [],
+      })) as string;
+
+      const tokenSymbol = (await client.readContract({
+        address: event.params.token_address as any,
+        abi: Erc20ABI,
+        functionName: "symbol",
+        args: [],
+      })) as string;
+
+      const tokenDecimals = (await client.readContract({
+        address: event.params.token_address as any,
+        abi: Erc20ABI,
+        functionName: "decimals",
+        args: [],
+      })) as number;
+      // let tokenSymbol = 
+
+      context.Token.set({
+        id: event.params.token_address,
+        address: event.params.token_address,
+        symbol: tokenSymbol,
+        name: tokenName,
+        decimals: BigInt(tokenDecimals),
+      });
+
+    }catch(error){
+      context.log.error(
+        `Unable to fetch token info ${event.params.token_address} | Record ${eventId}`
+      );
+    }
+  }
+
+  context.Redpacket.set(redpacketEntity)
 });
 
-HappyRedPacketContract.RefundSuccess.handler(({ event, context }) => {
-  const entity: HappyRedPacket_RefundSuccessEntity = {
-    id: `${event.transactionHash}_${event.logIndex}`,
-    event_id: event.params.id,
-    token_address: event.params.token_address,
-    remaining_balance: event.params.remaining_balance,
-    lock: event.params.lock,
-  };
+HappyRedPacketContract.RefundSuccess.loader(({event,context}) => {
+  context.Redpacket.load(event.params.id,undefined);
+})
 
-  context.HappyRedPacket_RefundSuccess.set(entity);
+HappyRedPacketContract.RefundSuccess.handler(({ event, context }) => {
+  let redpacket = context.Redpacket.get(event.params.id);
+  if (redpacket == undefined) {
+    return
+  }
+
+  let tempRefundId = event.transactionHash + event.logIndex
+
+  context.Refund.set({
+    id: tempRefundId,
+    happyRedPacketId: event.params.id,
+    tokenAddress: event.params.token_address,
+    remainingBalance: event.params.remaining_balance,
+    lock: event.params.lock,
+    blockNumber: BigInt(event.blockNumber),
+    blockTimestamp: BigInt(event.blockTimestamp),
+    transactionHash: event.transactionHash,
+  });
+
+  context.Redpacket.set({
+    ...redpacket,
+    refunded: true,
+    refunder_id: tempRefundId,
+    allClaimed: redpacket.allClaimed,
+  });
+
+  context.Lastupdate.set({
+    id: "1",
+    lastupdateTimestamp: BigInt(event.blockTimestamp),
+  })
 });
